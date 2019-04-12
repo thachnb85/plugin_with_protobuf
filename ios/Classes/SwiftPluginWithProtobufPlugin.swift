d915d14c886a4cb369d2bd2a04d9b5bdfbabb02d
thachnb85 <thachnb85@gmail.com>
2019-04-12 14:10:57 -0600

    Implement swift code to provide data to Dart

--- ios/Classes/SwiftPluginWithProtobufPlugin.swift
+++ ios/Classes/SwiftPluginWithProtobufPlugin.swift
@@ -1,6 +1,45 @@
 import Flutter
 import UIKit
 
+func getChildren() -> Person
+{
+    return Person.with{
+        $0.name = "Thach"
+        $0.addressesList = [
+            Address.with {
+                $0.streetName = "Stonechurch"
+                $0.city = "Hamilton"
+                $0.province = Province.on
+                $0.postalCode = 1000
+            }
+        ]
+        $0.childrenList = [
+            Person.with {
+                $0.name = "Liam"
+                $0.addressesList = [
+                    Address.with {
+                        $0.streetName = "Lakeridge"
+                        $0.city = "Hamilton"
+                        $0.province = Province.on
+                        $0.postalCode = 1000
+                    }
+                ]
+            },
+            Person.with {
+                $0.name = "Phong"
+                $0.addressesList = [
+                    Address.with {
+                        $0.streetName = "Lakeview"
+                        $0.city = "Hamilton"
+                        $0.province = Province.on
+                        $0.postalCode = 1000
+                    }
+                ]
+            },
+        ]
+    };
+}
+
 public class SwiftPluginWithProtobufPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "plugin_with_protobuf", binaryMessenger: registrar.messenger())
@@ -9,6 +48,20 @@ public class SwiftPluginWithProtobufPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    
+    if (call.method == "getPlatformVersion")
+    {
+        result("iOS " + UIDevice.current.systemVersion)
+    }
+    else if (call.method == "getMyChildren")
+    {
+        do{
+            result(try getChildren().serializedData());
+        }
+        catch
+        {
+            result(FlutterError(code: "Cannot serialize data", message: nil, details: nil))
+        }
+    }
   }
 }
